45d5a044168cb92cc682fc3024aa2f1e0dfb8b68
Gaenox <blue3eagle3naro@gmail.com>
2021-06-24 21:50:11 +0200

    d

---
+++ Google/JS practice/prac1.js
@@ -0,0 +1,26 @@
+/*let a;
+let b;
+
+function add(a, b){
+    var c = a+ b
+    console.log(c)
+}
+add(3,4)*/
+
+/*var a = 6;
+var b = 3;
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a**b)
+a = --b
+console.log(a)
+console.log(b)*/
+
+var a;
+while(isNaN(a)){
+    a = prompt('Enter a Number')
+    a = Number(a)
+    console.log(a, typeof a)
+}
