db9ffc58cc4136db610d77bb742c930cfa835456
Gaenox <blue3eagle3naro@gmail.com>
2021-06-24 21:51:07 +0200

    s

---
+++ JS practice/Untitled.js
@@ -0,0 +1,173 @@
+
+// let a;
+// let aa;
+// let aaa;
+
+
+// let abc = function add (a,b){
+//     console.log(a+b)
+// }
+
+// abc(3,4)
+
+
+// function stack (){
+//     stack()
+
+// }
+
+// stack()
+
+
+// let a = 5;
+// console.log(a)
+
+// {
+//     let a = 8;
+//     console.log(a)
+    
+// }
+
+// console.log(a)
+
+// var a = 6;
+// var b = 2;
+
+/* console.log(a+b) //8
+console.log(a-b) //4
+console.log(a*b) //12
+console.log(a/b) //3
+console.log(a**b) //36 */
+
+
+//ıncrement decrement
+// var a = 6;
+// var b = 3;
+
+// let a = --b // Bu satırda  b=2
+// console.log({b})  //bu satırda b=2
+// console.log({a})
+
+// let a = b-- // Bu satırda  b=3
+// console.log({b}) //bu satırda b=2
+// console.log({a})
+
+// var b = 5;
+
+// console.log(++b) //6
+// console.log(b++) // 5
+// console.log({b}) // 6
+
+// var a = ++b //b = b + 1, a =b
+// console.log({b}) //b =6
+// console.log({a})//6
+
+
+// a = b++ // a=b , b = b+1
+// console.log({b}) //6
+// console.log({a})//5
+
+// b = 6;
+// a =2;
+
+// console.log(b%a)
+
+
+//Assignment Operatos
+
+// var a = 6;
+// var b = 2;
+
+// console.log(a=b, {a},{b})
+// console.log(a+=b, {a},{b}) 
+// console.log(a-=b, {a},{b})
+// console.log(a/=b, {a},{b})
+// console.log(a*=b, {a},{b})
+// console.log(a%=b, {a},{b})
+
+// var a = 2;
+// var b = '2';
+
+// console.log('strict equality',a === b) //false
+// console.log('equality',a == b) //true
+
+
+// var a = 2;
+// var b = '2';
+
+// console.log('strict inequality',a !== b) // true
+// console.log('inequality',a != b) //false
+
+// let age = 14;
+
+// console.log(age >= 14) //true
+// console.log(age > 14) //false
+
+
+// console.log(false == false) //true
+// console.log(Boolean(0) == false) //true
+// console.log(Boolean('0') == false) //false
+// console.log(0 == false) // true
+// console.log('0' == false) //true
+
+// console.log(typeof null) //object
+// console.log(null == false) //false
+// console.log(null == true) //false
+// console.log(null+3) //3
+// console.log(null == 0) //false
+// console.log(Boolean(null) == false) //true
+// console.log(Boolean(null) == Boolean(0)) //true
+
+// console.log(typeof NaN)
+
+//isNaN
+
+// Kullanıcıdan alınan veri sayı mmı değil mi?
+// var a ;
+
+// while(isNaN(a)){
+//     a = prompt("Enter a Number")
+//     a = Number(a)
+//     console.log(a, typeof a)
+// }
+
+// console.log(015+025) //34 = 13 + 21
+// console.log(15+025) //36 = 15 + 21
+// console.log(015+25) //38 = 13 + 25
+// console.log(15+25) //40
+
+// console.log(0015+0025)
+
+// 0 1 2 3 4 5 6 7
+
+// console.log(9 < 8) //false
+// console.log( 9 < 8 < 7 ) // true
+// console.log(false < 7) //true
+
+// console.log('Clarusway'> 'clarusway') //false
+console.log('A'.charCodeAt())
+console.log('a'.charCodeAt())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
