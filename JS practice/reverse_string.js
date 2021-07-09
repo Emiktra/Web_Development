9152dad74a3f3cba812fa07dcf8763141feb078b
Gaenox <blue3eagle3naro@gmail.com>
2021-07-09 20:20:27 +0200

    css halfway done

--- JS practice/reverse_string.js
+++ JS practice/reverse_string.js
@@ -3,5 +3,8 @@ function reverseString(a) {
     a = a.reverse()
     return a.join('')
 }
+function reverseString2(a) {
+    return a.split('').reverse.join('')
+}
 var string = prompt('Enter word/sentence to be reversed')
 console.log(reverseString(string))
