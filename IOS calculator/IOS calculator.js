9b3e99b90a3a5bb651512663cda7cb659bf2dae2
Gaenox <blue3eagle3naro@gmail.com>
2021-07-15 23:33:21 +0200

    broken calculator

---
+++ IOS calculator/IOS calculator.js
@@ -0,0 +1,111 @@
+const numpad = document.getElementById('numpad')
+var result = []
+var visual = ""
+numpad.children[0].addEventListener('click', function(){
+    result = []
+    visual = ""
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[1].addEventListener('click', function(){
+    result.push("/100")
+    visual += "%"
+    document.getElementById('output').innerHTML = visual
+})    
+numpad.children[2].addEventListener('click', function(){
+    result.push("/")
+    visual += "÷"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[3].addEventListener('click', function (){
+    result.pop()
+    visual = visual.substring(0, visual.length -1)
+    console.log(result)
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[4].addEventListener('click', function (){
+    result.push("7")
+    visual += "7"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[5].addEventListener('click', function (){
+    result.push("8")
+    visual += "8"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[6].addEventListener('click', function (){
+    result.push("9")
+    visual += "9"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[7].addEventListener('click', function (){
+    result.push("*")
+    visual += "×"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[8].addEventListener('click', function (){
+    result.push("4")
+    visual += "4"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[9].addEventListener('click', function (){
+    result.push("5")
+    visual += "5"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[10].addEventListener('click', function (){
+    result.push("6")
+    visual += "6"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[11].addEventListener('click', function (){
+    result.push("-")
+    visual += "-"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[12].addEventListener('click', function (){
+    result.push("1")
+    visual += "1"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[13].addEventListener('click', function (){
+    result.push("2")
+    visual += "2"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[14].addEventListener('click', function (){
+    result.push("3")
+    visual += "3"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[15].addEventListener('click', function (){
+    result.push("+")
+    visual += "+"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[16].addEventListener('click', function (){
+    result.push("0")
+    visual += "0"
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[17].addEventListener('click', function (){
+    result.push(".")
+    visual += "."
+    document.getElementById('output').innerHTML = visual
+})
+numpad.children[18].addEventListener('click', function (){
+    for (i=0; i < result.length; i++) {
+        if (result[i] == "/100"){
+            for (e=i -1; e > 0; e--){
+                console.log(e)
+                if (isNaN(result[e])){
+                    result.splice(e +1, 0, "(")
+                    result.splice(result.length +1, 0, ")")
+                    console.log(result)
+                    break
+                }
+            }
+        }
+    }
+    document.getElementById('output').innerHTML = eval(result.join(''))
+    visual = ""
+})
