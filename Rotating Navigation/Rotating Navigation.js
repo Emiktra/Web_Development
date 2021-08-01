724ff3e7d61a279d09379c3fb7b9490cbf94eb40
Gaenox <blue3eagle3naro@gmail.com>
2021-08-01 17:19:19 +0200

    rotating navigation project done

--- Rotating Navigation/Rotating Navigation.js
+++ Rotating Navigation/Rotating Navigation.js
@@ -1,3 +1,19 @@
 const mainPage = document.querySelector("nav")
+const closeBtn = document.querySelector("body > span")
+const navigation = document.querySelector("footer > ul")
 mainPage.addEventListener("click", function(){
-    document.querySelector("body div").classList.toggle("rotate")})
+    document.querySelector("body > div").classList.toggle("rotate")
+    document.querySelector("body > span").classList.toggle("notClosed")
+    for (i = 0; i < navigation.children.length; i++){
+        navigation.children[i].id = `a${i +1}`
+    }
+    document.querySelector("body > footer").classList.toggle("active")
+})
+closeBtn.addEventListener("click", function(){
+    document.querySelector("body > div").classList.toggle("rotate")
+    document.querySelector("body > span").classList.toggle("notClosed")
+    for (i = 0; i < navigation.children.length; i++){
+        navigation.children[i].id = `default${i +1}`
+    }
+    document.querySelector("body > footer").classList.toggle("active")
+})
