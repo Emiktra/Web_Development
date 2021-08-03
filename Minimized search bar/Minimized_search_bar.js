923374d2dd47250726d9ae833b1db495d8f03552
Gaenox <blue3eagle3naro@gmail.com>
2021-08-03 20:29:06 +0200

    expanding search bar done

--- Minimized search bar/Minimized_search_bar.js
+++ Minimized search bar/Minimized_search_bar.js
@@ -3,17 +3,39 @@ const SearchIcon = document.getElementById("searchBtn");
 const InputArea = document.getElementById("InputArea");
 const cleanBtn = document.getElementById("cleanBtn");
 
-SearchIcon.addEventListener("click", function(defaultChecker){
+//Setting Default
+Bar.classList.toggle("closed")
+cleanBtn.classList.toggle("btnClosed")
+InputArea.classList.toggle("inputClosed")
+InputArea.classList.toggle("inputClosed2")
+
+SearchIcon.addEventListener("click", searc)
+InputArea.addEventListener("keypress", function(e){
+    if (e.key === "Enter"){
+        searc()
+    }
+})
+function searc() {
     if (InputArea.value == ""){
-        Bar.classList.toggle("open")
-        InputArea.classList.toggle("inputClosed")
-        cleanBtn.classList.toggle("btnClosed")
+        Bar.classList.toggle("closed")
+        if (Bar.className.includes("closed")){
+            InputArea.classList.toggle("inputClosed")
+            cleanBtn.classList.toggle("btnClosed")
+            setTimeout(function(){
+                InputArea.classList.toggle("inputClosed2")
+            }, 180)
+        } else {
+            InputArea.classList.toggle("inputClosed2")
+            setTimeout(function(){
+                InputArea.classList.toggle("inputClosed")
+                cleanBtn.classList.toggle("btnClosed")
+            }, 0)
+        }
     } else if(!InputArea.value == ""){
-        defaultChecker.preventDefault();
         window.location.href = "https://www.google.com/search?q=" + InputArea.value
         InputArea.value = ""
     }
-})
+}
 cleanBtn.addEventListener("click", function(){
     InputArea.value = ""
 })
